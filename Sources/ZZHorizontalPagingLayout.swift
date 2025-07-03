5bee14eb4d42efccfaf608d1c999b190a6f00d1b
MartyChristopher <519483040@qq.com>
2025-07-03 09:41:55 +0800

    no message

--- Sources/ZZHorizontalPagingLayout.swift
+++ Sources/ZZHorizontalPagingLayout.swift
@@ -1,6 +1,7 @@
 //
 //  ZZHorizontalPagingLayout.swift
 //
+//  水平滚动翻页
 
 import UIKit
 
@@ -14,18 +15,24 @@ public class ZZHorizontalPagingLayout: UICollectionViewFlowLayout {
         scrollDirection = .horizontal
         
         guard let collectionView = collectionView else { return }
-        let itemInset = collectionView.frame.size.width - itemSize.width
-        
+        setInterpageSpacing(for: collectionView)
+        setPagingOrigin(for: collectionView)
+    }
+    
+    private func setInterpageSpacing(for collectionView: UICollectionView) {
         if collectionView.responds(to: NSSelectorFromString("_setInterpageSpacing:")) == true {
             let selector = NSSelectorFromString("_setInterpageSpacing:")
             if let method = class_getInstanceMethod(type(of: collectionView), selector) {
                 typealias MyFunction = @convention(c) (AnyObject, Selector, CGSize) -> Void
                 let function = unsafeBitCast(method_getImplementation(method), to: MyFunction.self)
+                let itemInset = collectionView.frame.size.width - itemSize.width
                 let size = CGSize(width: -(itemInset - minimumLineSpacing), height: 0)
                 function(collectionView, selector, size)
             }
         }
-        
+    }
+    
+    private func setPagingOrigin(for collectionView: UICollectionView) {
         if collectionView.responds(to: NSSelectorFromString("_setPagingOrigin:")) == true {
             let selector = NSSelectorFromString("_setPagingOrigin:")
             if let method = class_getInstanceMethod(type(of: collectionView), selector) {
@@ -40,8 +47,9 @@ public class ZZHorizontalPagingLayout: UICollectionViewFlowLayout {
         guard let collectionView = collectionView,
               let attributesArray = super.layoutAttributesForElements(in: rect) else { return [] }
         let visibleRect = CGRect(x: collectionView.contentOffset.x, y: collectionView.contentOffset.y, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
+        let horizontalCenter = visibleRect.origin.x + visibleRect.size.width / 2.0
         for attributes in attributesArray {
-            let distance = (visibleRect.origin.x + visibleRect.size.width / 2.0) - attributes.center.x
+            let distance = horizontalCenter - attributes.center.x
             let normalizedDistance = abs(distance / itemSize.width)
             let zoom = 1 - zoomRatio * normalizedDistance
             attributes.transform3D = CATransform3DMakeScale(zoom, zoom, 1.0)
--- Sources/ZZVerticalPagingLayout.swift
+++ Sources/ZZVerticalPagingLayout.swift
@@ -1,6 +1,7 @@
 //
 //  ZZVerticalPagingLayout.swift
 //
+//  竖直滚动翻页
 
 import UIKit
 
@@ -14,18 +15,24 @@ public class ZZVerticalPagingLayout: UICollectionViewFlowLayout {
         scrollDirection = .vertical
         
         guard let collectionView = collectionView else { return }
-        let itemInset = collectionView.frame.size.height - itemSize.height
-        
+        setInterpageSpacing(for: collectionView)
+        setPagingOrigin(for: collectionView)
+    }
+    
+    private func setInterpageSpacing(for collectionView: UICollectionView) {
         if collectionView.responds(to: NSSelectorFromString("_setInterpageSpacing:")) == true {
             let selector = NSSelectorFromString("_setInterpageSpacing:")
             if let method = class_getInstanceMethod(type(of: collectionView), selector) {
                 typealias MyFunction = @convention(c) (AnyObject, Selector, CGSize) -> Void
                 let function = unsafeBitCast(method_getImplementation(method), to: MyFunction.self)
+                let itemInset = collectionView.frame.size.height - itemSize.height
                 let size = CGSize(width: 0, height: -(itemInset - minimumLineSpacing))
                 function(collectionView, selector, size)
             }
         }
-        
+    }
+    
+    private func setPagingOrigin(for collectionView: UICollectionView) {
         if collectionView.responds(to: NSSelectorFromString("_setPagingOrigin:")) == true {
             let selector = NSSelectorFromString("_setPagingOrigin:")
             if let method = class_getInstanceMethod(type(of: collectionView), selector) {
@@ -40,8 +47,9 @@ public class ZZVerticalPagingLayout: UICollectionViewFlowLayout {
         guard let collectionView = collectionView,
               let attributesArray = super.layoutAttributesForElements(in: rect) else { return [] }
         let visibleRect = CGRect(x: collectionView.contentOffset.x, y: collectionView.contentOffset.y, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
+        let verticalCenter = visibleRect.origin.y + visibleRect.size.height / 2.0
         for attributes in attributesArray {
-            let distance = (visibleRect.origin.y + visibleRect.size.height / 2.0) - attributes.center.y
+            let distance = verticalCenter - attributes.center.y
             let normalizedDistance = abs(distance / itemSize.height)
             let zoom = 1 - zoomRatio * normalizedDistance
             attributes.transform3D = CATransform3DMakeScale(zoom, zoom, 1.0)
